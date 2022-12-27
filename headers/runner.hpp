45a920ebcc6cc6f073456e81dbdfec142f85b200
vicviod <vicviodlogin@gmail.com>
2022-12-27 17:13:57 -0500

    fix argument passing string to run

--- headers/runner.hpp
+++ headers/runner.hpp
@@ -1,1 +1,3 @@
-void run(int only_sid = -1, int maxdepth = -1);
+#include <string>
+
+void run(int only_sid = -1, int maxdepth = -1, std::string directory = "evaluation");
--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,11 @@ int main(int argc, char**argv) {
   //bruteSolve();
   int only_sid = -1;
   printf("arguments to main %s", argv[1]); // VICHANGE
-  string directory = argv[1] // VICHANGE add directory argument
+  string directory = argv[1]; // VICHANGE add directory argument
   if (argc >= 3) { // VICHANGE: push arguments up by one
     only_sid = atoi(argv[2]); // VICHANGE
     printf("Running only task # %d\n", only_sid);
-   printf("second argument %s", argv[2]); // VICHANGE
+    printf("second argument %s", argv[2]); // VICHANGE
   }
   int maxdepth = -1;
   if (argc >= 4) { // VICHANGE
