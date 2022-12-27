9403883c1836d71dd934c0e5f8d4936902f34289
vicviod <vicviodlogin@gmail.com>
2022-12-27 17:05:37 -0500

    replace cout with printf

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,18 @@ int main(int argc, char**argv) {
   //bruteSubmission();
   //bruteSolve();
   int only_sid = -1;
-  cout << "arguments to main" << argv[1]; // VICHANGE
+  printf("arguments to main %s", argv[1]); // VICHANGE
   string directory = argv[1] // VICHANGE add directory argument
   if (argc >= 3) { // VICHANGE: push arguments up by one
     only_sid = atoi(argv[2]); // VICHANGE
     printf("Running only task # %d\n", only_sid);
-    cout << argv[2]; // VICHANGE
+   printf("second argument %s", argv[2]); // VICHANGE
   }
   int maxdepth = -1;
   if (argc >= 4) { // VICHANGE
     maxdepth = atoi(argv[3]); // VICHANGE
     printf("Using max depth %d\n", maxdepth);
-    cout << argv[3]; // VICHANGE
+    printf("third argument %s", argv[3]); // VICHANGE
   }
   run(only_sid, maxdepth, directory);
 }
